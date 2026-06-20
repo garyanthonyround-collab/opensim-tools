4b7932689a904e130ddd51e34f48983b370e8b2a
Gary Round <gary.anthony.round@gmail.com>
2026-06-20 23:29:10 +0100

    Fix TerrainProject centre reference test

--- tests/test_terrain_project.py
+++ tests/test_terrain_project.py
@@ -9,4 +9,4 @@ def test_terrain_project_stores_centre_reference():
 def test_terrain_project_parses_centre_reference():
     project = TerrainProject().centre("NY4452")
 
-    assert project.centre_point == (344000, 520000)
+    assert project.centre_point == (344000, 552000)
